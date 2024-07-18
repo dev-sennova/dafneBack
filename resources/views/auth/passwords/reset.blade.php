0a60dcee0de05e7f5abdcf464d495f5ce42c0bc4
DarwingR27 <145625475+DarwingR27@users.noreply.github.com>
2024-07-18 08:13:12 -0500

    DAFNEBACK-9-Ajustes-Finales (#6)
    Co-authored-by: Darwing27 <dsrondon96@misena.edu.co>

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="reset-password-container mt-5">
-    <div class="reset-password-title">{{ __('Reestablecer Contraseña') }}</div>
+    <div class="reset-password-title">{{ __('Restablecer Contraseña') }}</div>
     <form method="POST" action="{{ route('password.update') }}" class="reset-password-form">
         @csrf
         <input type="hidden" name="token" value="{{ $token }}">
